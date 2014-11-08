d60368360d70456da1faae1266457f499fa9d789
Ben Wu <ben@intrepid.io>
2014-11-08 12:37:51 -0500

    Initial project file

---
+++ WeNeed/WeNeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeNeed
+//
+//  Created by Benjamin Wu on 11/8/14.
+//  Copyright (c) 2014 Donner Dungeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeNeed/WeNeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeNeed
+//
+//  Created by Benjamin Wu on 11/8/14.
+//  Copyright (c) 2014 Donner Dungeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
