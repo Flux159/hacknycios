951183f6d94edde425d9141dfdf5584b165e0a2c
David Zheng <davidzheng@Davids-MacBook-Pro-3.local>
2014-11-10 22:04:37 -0500

    added leaf pile animations

--- WeNeed/WeNeed/Views/Items/IGTTreesView.m
+++ WeNeed/WeNeed/Views/Items/IGTTreesView.m
@@ -8,6 +8,12 @@
 
 #import "IGTTreesView.h"
 
+@interface IGTTreesView()
+
+@property (nonatomic, strong) UIImageView *leafPileView;
+
+@end
+
 @implementation IGTTreesView
 
 - (instancetype)initWithFrame:(CGRect)frame {
@@ -43,14 +49,35 @@
 }
 
 -(void)setupLeafPile{
-//    NSString *leafPileName = @"leaf-pile-1";
-    UIImageView *leafPileView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"leaf-pile-1"]];
-    leafPileView.frame = CGRectMake(-60, self.frame.size.height - 172, self.frame.size.width+120, 172);
-    [self.backgroundView addSubview:leafPileView];
+    self.leafPileView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"leaf-pile-1"]];
+    self.leafPileView.frame = CGRectMake(-60, self.frame.size.height - 172, self.frame.size.width+120, 172);
+    [self.backgroundView addSubview:self.leafPileView];
+    
+    [self startLeafPileCycle];
 }
 
--(void)animateLeafPile{
+-(void)startLeafPileCycle{
+    NSTimeInterval delay = 15.0;
+    for (int i = 2; i <= 4; i++) {
+        [self performSelector:@selector(transitionLeavesToState:) withObject:@(i) afterDelay:delay];
+        delay += 15.0;
+    }
+}
+
+-(void)transitionLeavesToState:(NSNumber*)state{
+    NSString *leafPileImage = [NSString stringWithFormat:@"leaf-pile-%i", [state intValue]];
+    UIImageView *tempLeafPile = [[UIImageView alloc] initWithImage:self.leafPileView.image];
+    tempLeafPile.frame = self.leafPileView.frame;
+    [self.backgroundView addSubview:tempLeafPile];
     
+    self.leafPileView.alpha = 0.0;
+    self.leafPileView.image = [UIImage imageNamed:leafPileImage];
+    
+    [UIView animateWithDuration:2.0 animations:^{
+        self.leafPileView.alpha = 1.0;
+    } completion:^(BOOL finished) {
+        [tempLeafPile removeFromSuperview];
+    }];
 }
 
 - (void)createAndAnimateLeaves {
@@ -76,8 +103,21 @@
                      animations:^{
                          leafView.frame = bottomFrame;
                      } completion:^(BOOL finished) {
-                         [leafView removeFromSuperview];
+                         [UIView animateWithDuration:1.0
+                                               delay:2.0
+                                             options:0
+                                          animations:^{
+                                              leafView.alpha = 0.0;
+                                            }
+                                          completion:^(BOOL finished) {
+                             [leafView removeFromSuperview];
+                         }];
+                         
                      }];
+    
+}
+
+- (void)stopAnimation{
     
 }
 
